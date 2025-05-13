70f3c470c082f4178c7bf54f4b0b45934d06df21
shians <registertonysu@gmail.com>
2025-05-13 11:36:01 +1000

    Parallelised test_maf

--- R/test_maf.R
+++ R/test_maf.R
@@ -1,20 +1,27 @@
 test_maf <- function(x, p = 0.10) {
     # validate input
     stopifnot(is(x, "data.frame"))
-    stopifnot(all(c("ref_count", "alt_count", "total_count") %in% colnames(x)))
+
+    req_cols <- c("ref_count", "alt_count", "total_count")
+    missing_cols <- setdiff(req_cols, colnames(x))
+    if (length(missing_cols) > 0) {
+        stop(glue::glue("Missing required columns: {paste(missing_cols, collapse = ', ')}"))
+    }
 
     minor_allele_count <- pmin(x$ref_count, x$alt_count)
 
-    p_val <- purrr::map2_dbl(
+    p_val <- furrr::future_map2_dbl(
         minor_allele_count,
         ceiling(x$total_count/2),
         ~binom.test(.x, .y, p, alternative = "greater")$p.value
     )
 
-    x %>%
+    result <- x %>%
         dplyr::mutate(
             minor_allele_count = minor_allele_count,
             p_val = p_val,
             adj_p_val = p.adjust(p_val, method = "BH")
         )
+
+    return(result)
 }
