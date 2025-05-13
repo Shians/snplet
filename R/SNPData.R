b9c733bd03db9df3b9eaa7f4c715b6b595dba5b3
shians <registertonysu@gmail.com>
2025-05-13 11:35:37 +1000

    Added filtering functions

--- R/SNPData.R
+++ R/SNPData.R
@@ -163,55 +163,67 @@ setMethod("cell_count_df", signature(x = "SNPData"),
 setGeneric("donor_count_df", function(x) standardGeneric("donor_count_df"))
 setMethod("donor_count_df", signature(x = "SNPData"),
     function(x) {
-        ref_count_grouped <- groupedRowSums(ref_count(x), get_sample_info(x)$donor_id)
-        alt_count_grouped <- groupedRowSums(alt_count(x), get_sample_info(x)$donor_id)
+        logger::log_info("Calculating donor level counts")
 
+        logger::log_info("Extracting reference counts")
+        ref_count_grouped <- groupedRowSums(ref_count(x), get_sample_info(x)$donor)
         ref_count_df <- ref_count_grouped %>%
             tibble::as_tibble() %>%
             dplyr::select(-any_of(c("unassigned", "doublet"))) %>%
             dplyr::mutate(snp_id = rownames(ref_count_grouped), .before = 1) %>%
-            tidyr::pivot_longer(contains("donor"), names_to = "donor_id", values_to = "ref_count")
+            tidyr::pivot_longer(contains("donor"), names_to = "donor", values_to = "ref_count")
 
+        logger::log_info("Extracting alternate counts")
+        alt_count_grouped <- groupedRowSums(alt_count(x), get_sample_info(x)$donor)
         alt_count_df <- alt_count_grouped %>%
             tibble::as_tibble() %>%
             dplyr::select(-any_of(c("unassigned", "doublet"))) %>%
             dplyr::mutate(snp_id = rownames(alt_count_grouped), .before = 1) %>%
-            tidyr::pivot_longer(contains("donor"), names_to = "donor_id", values_to = "alt_count")
+            tidyr::pivot_longer(contains("donor"), names_to = "donor", values_to = "alt_count")
 
-        dplyr::inner_join(ref_count_df, alt_count_df, by = c("snp_id", "donor_id")) %>%
+        logger::log_info("Processing reference and alternate counts")
+        out <- dplyr::inner_join(ref_count_df, alt_count_df, by = c("snp_id", "donor")) %>%
             dplyr::mutate(
                 total_count = ref_count + alt_count,
                 ref_ratio = ref_count / total_count,
                 maf = pmin(ref_count, alt_count) / total_count
-            )
+            ) %>%
+            dplyr::filter(total_count > 0)
+
+        logger::log_success("Donor level counts calculated")
+        out
     }
 )
 
 setGeneric("clonotype_count_df", function(x) standardGeneric("clonotype_count_df"))
 setMethod("clonotype_count_df", signature(x = "SNPData"),
     function(x) {
-        ref_count_grouped <- groupedRowSums(ref_count(x), get_sample_info(x)$clonotype_id)
-        alt_count_grouped <- groupedRowSums(alt_count(x), get_sample_info(x)$clonotype_id)
+        logger::log_info("Calculating clonotype level counts")
 
+        logger::log_info("Extracting reference counts")
+        ref_count_grouped <- groupedRowSums(ref_count(x), get_sample_info(x)$clonotype)
         ref_count_df <- ref_count_grouped %>%
             tibble::as_tibble() %>%
             dplyr::mutate(snp_id = rownames(ref_count_grouped), .before = 1) %>%
             tidyr::pivot_longer(contains("clonotype"), names_to = "clonotype", values_to = "ref_count")
 
+        logger::log_info("Extracting alternate counts")
+        alt_count_grouped <- groupedRowSums(alt_count(x), get_sample_info(x)$clonotype)
         alt_count_df <- alt_count_grouped %>%
             tibble::as_tibble() %>%
             dplyr::mutate(snp_id = rownames(alt_count_grouped), .before = 1) %>%
             tidyr::pivot_longer(contains("clonotype"), names_to = "clonotype", values_to = "alt_count")
 
+        logger::log_info("Processing reference and alternate counts")
         most_likely_donor <- get_sample_info(x) %>%
-            dplyr::filter(!is.na(clonotype_id) & !is.na(donor_id)) %>%
-            dplyr::select(clonotype_id, donor_id) %>%
-            dplyr::count(donor_id, clonotype_id) %>%
-            dplyr::arrange(dplyr::desc(n), .by = clonotype_id) %>%
-            dplyr::slice(1, .by = clonotype_id) %>%
-            dplyr::select(clonotype_id, donor_id)
+            dplyr::filter(!is.na(clonotype) & !is.na(donor)) %>%
+            dplyr::select(clonotype, donor) %>%
+            dplyr::count(donor, clonotype) %>%
+            dplyr::arrange(dplyr::desc(n), .by = clonotype) %>%
+            dplyr::slice(1, .by = clonotype) %>%
+            dplyr::select(clonotype, donor)
 
-        dplyr::inner_join(ref_count_df, alt_count_df, by = c("snp_id", "clonotype")) %>%
+        out <- dplyr::inner_join(ref_count_df, alt_count_df, by = c("snp_id", "clonotype")) %>%
             dplyr::mutate(
                 total_count = ref_count + alt_count,
                 ref_ratio = ref_count / total_count,
@@ -219,7 +231,91 @@ setMethod("clonotype_count_df", signature(x = "SNPData"),
             ) %>%
             dplyr::left_join(
                 most_likely_donor,
-                by = c("clonotype" = "clonotype_id")
-            )
+                by = c("clonotype" = "clonotype")
+            ) %>%
+            dplyr::filter(total_count > 0)
+
+        logger::log_success("Clonotype level counts calculated")
+        out
+    }
+)
+
+#' Filter SNPData object by SNP information
+#'
+#' @param .data A SNPData object
+#' @param ... Logical expressions to filter by, based on snp_info columns
+#' @return A new filtered SNPData object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Filter SNPs with coverage > 10
+#' filtered_snps <- filter_snps(snp_data, coverage > 10)
+#'
+#' # Filter SNPs with multiple conditions
+#' filtered_snps <- filter_snps(snp_data, coverage > 10, non_zero_samples > 5)
+#' }
+setGeneric("filter_snps", function(.data, ...) standardGeneric("filter_snps"))
+setMethod("filter_snps", signature(.data = "SNPData"),
+    function(.data, ...) {
+        # Capture NSE expressions
+        dots <- rlang::enquos(...)
+
+        # Apply filter to snp_info
+        selected_snps <- .data@snp_info %>%
+            dplyr::filter(!!!dots)
+
+        # Get indices of selected SNPs
+        selected_indices <- match(selected_snps$snp_id, rownames(.data@alt_count))
+
+        snp_total <- nrow(.data)
+        snp_selected <- length(selected_indices)
+        snp_removed <- snp_total - snp_selected
+        removed_perc <- scales::percent(snp_removed / snp_total, accuracy = 0.01)
+        logger::log_info("{snp_removed} ({removed_perc}) SNPs removed. {snp_selected} SNPs remaining.")
+        # Subset the SNPData object
+        .data[selected_indices, ]
+    }
+)
+
+#' Filter SNPData object by sample/cell/barcode information
+#'
+#' @param .data A SNPData object
+#' @param ... Logical expressions to filter by, based on sample_info columns
+#' @return A new filtered SNPData object
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Filter cells with library size > 1000
+#' filtered_cells <- filter_barcodes(snp_data, library_size > 1000)
+#'
+#' # Filter cells from a specific donor
+#' filtered_cells <- filter_barcodes(snp_data, donor_id == "donor_1")
+#'
+#' # Filter cells with multiple conditions
+#' filtered_cells <- filter_barcodes(snp_data, library_size > 1000, non_zero_snps > 50)
+#' }
+setGeneric("filter_barcodes", function(.data, ...) standardGeneric("filter_barcodes"))
+setMethod("filter_barcodes", signature(.data = "SNPData"),
+    function(.data, ...) {
+        # Capture NSE expressions
+        dots <- rlang::enquos(...)
+
+        # Apply filter to sample_info
+        selected_samples <- .data@sample_info %>%
+            dplyr::filter(!!!dots)
+
+        # Get indices of selected samples
+        selected_indices <- match(selected_samples$cell_id, colnames(.data@alt_count))
+
+        sample_total <- ncol(.data)
+        sample_selected <- length(selected_indices)
+        sample_removed <- sample_total - sample_selected
+        removed_perc <- scales::percent(sample_removed / sample_total, accuracy = 0.01)
+        logger::log_info("{sample_removed} ({removed_perc}) barcodes removed. {sample_selected} barcodes remaining.")
+
+        # Subset the SNPData object
+        .data[, selected_indices]
     }
 )
---
+++ R/remove.R
@@ -0,0 +1,141 @@
+#' Remove doublet cells from a SNPData object
+#'
+#' This function filters out cells that are identified as doublets from a SNPData object.
+#' Doublets are identified based on the 'donor' column in the sample_info slot, where
+#' cells labeled as 'doublet' are removed.
+#'
+#' @param x A SNPData object
+#'
+#' @return A filtered SNPData object with doublets removed
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Remove doublets from SNPData object
+#' filtered_data <- remove_doublets(snp_data)
+#' }
+remove_doublets <- function(x, drop_na = TRUE) {
+    # Check that x is a SNPData object
+    if (!methods::is(x, "SNPData")) {
+        stop("Input must be a SNPData object")
+    }
+
+    # Get sample info
+    sample_info <- get_sample_info(x)
+
+    # Check if donor column exists
+    if (!"donor" %in% colnames(sample_info)) {
+        warning("No 'donor' column found in sample_info, returning original object")
+        return(x)
+    }
+
+    # Identify doublets
+    cells_to_remove <- sample_info$donor == "doublet"
+
+    # Handle NA values
+    if (drop_na) {
+        cells_to_remove[is.na(cells_to_remove)] <- TRUE
+    } else {
+        cells_to_remove[is.na(cells_to_remove)] <- FALSE
+    }
+
+    barcodes_total <- ncol(x)
+    barcodes_removed <- sum(cells_to_remove)
+    barcodes_remaining <- barcodes_total - barcodes_removed
+    removed_perc <- scales::percent(barcodes_removed / barcodes_total, accuracy = 0.01)
+    logger::log_info("{barcodes_removed} ({removed_perc}) doublet barcodes removed. {barcodes_remaining} barcodes remaining.")
+
+    # Return filtered data
+    return(x[, !cells_to_remove])
+}
+
+#' Remove SNPs with NA gene names
+#'
+#' This function removes SNPs that have NA values in the gene_name column
+#' of the snp_info slot. This is useful for focusing analysis on SNPs
+#' that are associated with known genes.
+#'
+#' @param x A SNPData object
+#' @param gene_col The column name in snp_info containing gene names (default: "gene_name")
+#'
+#' @return A filtered SNPData object with NA gene SNPs removed
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Filter out SNPs with NA gene names
+#' filtered_data <- remove_na_genes(snp_data)
+#' }
+remove_na_genes <- function(x, gene_col = "gene_name") {
+    # Check that x is a SNPData object
+    if (!methods::is(x, "SNPData")) {
+        stop("Input must be a SNPData object")
+    }
+
+    # Get SNP info
+    snp_info <- get_snp_info(x)
+
+    # Check if gene column exists
+    if (!gene_col %in% colnames(snp_info)) {
+        warning(paste0("No '", gene_col, "' column found in snp_info, returning original object"))
+        return(x)
+    }
+
+    # Identify SNPs with non-NA gene names
+    snps_to_keep <- !is.na(snp_info[[gene_col]])
+
+    snps_total <- nrow(snp_info)
+    snps_removed <- sum(!snps_to_keep)
+    snps_remaining <- snps_total - snps_removed
+    removed_perc <- scales::percent(snps_removed / snps_total, accuracy = 0.01)
+    logger::log_info("{snps_removed} ({removed_perc}) SNPs with NA gene_name values removed. {snps_remaining} SNPs remaining.")
+
+    # Return filtered data
+    return(x[snps_to_keep, ])
+}
+
+#' Remove barcodes with NA clonotype values
+#'
+#' This function filters out barcodes that have NA values in the clonotype column
+#' of the sample_info slot. This is useful for analyses that require valid
+#' clonotype assignments for all barcodes.
+#'
+#' @param x A SNPData object
+#' @param clonotype_col The column name in sample_info containing clonotype values (default: "clonotype")
+#'
+#' @return A filtered SNPData object with NA clonotype barcodes removed
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Remove barcodes with NA clonotype values
+#' filtered_data <- remove_na_clonotypes(snp_data)
+#' }
+remove_na_clonotypes <- function(x, clonotype_col = "clonotype") {
+    # Check that x is a SNPData object
+    if (!methods::is(x, "SNPData")) {
+        stop("Input must be a SNPData object")
+    }
+
+    # Get sample info
+    sample_info <- get_sample_info(x)
+
+    # Check if clonotype column exists
+    if (!clonotype_col %in% colnames(sample_info)) {
+        warning(paste0("No '", clonotype_col, "' column found in sample_info, returning original object"))
+        return(x)
+    }
+
+    # Identify barcodes with NA clonotype values
+    barcodes_to_remove <- is.na(sample_info[[clonotype_col]])
+
+    clonotypes_total <- nrow(sample_info)
+    clonotypes_removed <- sum(barcodes_to_remove)
+    clonotypes_remaining <- clonotypes_total - clonotypes_removed
+    removed_perc <- scales::percent(clonotypes_removed / clonotypes_total, accuracy = 0.01)
+    logger::log_info("{clonotypes_removed} ({removed_perc}) barcodes with NA clonotype values removed. {clonotypes_remaining} barcodes remaining.")
+
+    # Return filtered data
+    return(x[, !barcodes_to_remove])
+}
+
